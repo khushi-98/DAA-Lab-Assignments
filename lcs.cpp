f7336c777df106a93bbbd08c724f1f7dccc8cc42
Khushi Bansal <102850725+khushi-98@users.noreply.github.com>
2024-05-08 07:48:43 +0530

    Update lcs.cpp

--- lcs.cpp
+++ lcs.cpp
@@ -23,19 +23,5 @@ int main(){
 }
 
 
-//#include<iostream>
-//using namespace std;
-//int lcs(string x, string y, int m, int n){
-//	if(m==0 || n==0){
-//		return 0;
-//	}
-//	if(x[m-1]==y[n-1]){
-//		return 1+lcs(x,y,m-1,n-1);
-//	}
-//	else{
-//		return 
-//	}
-//}
-
 
 
